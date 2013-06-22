dca4babf165f68c345da2c8db3cc079699c5f92c
Hidabe <fhidalgo@sopinet.com>
2013-06-22 17:34:09 +0200

    fixed error

--- lib/Sopinet/Colorize/ColorizeService.php
+++ lib/Sopinet/Colorize/ColorizeService.php
@@ -19,6 +19,6 @@ class ColorizeService
 	static public function autoColorizeFromString($string_css, $image) {
 		$colors_css = ColorizeHelper::getColorsFromStringCss($string_css);
 		$colors_img = ColorizeHelper::getColorsFromImage($image);
-		return ColorizeHelper::paintCssWithColors($string_css);
+		return ColorizeHelper::paintCssWithColors($string_css, $colors_css, $colors_img);
 	}
 }
